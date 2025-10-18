8331a21bc17f7386fbc65160672f535778277b97
Richard Pedersen <richardpedersen3@gmail.com>
2025-10-18 06:37:41 -0600

    simple subgraph indexer for identity registry

---
+++ src/erc-721.ts
@@ -0,0 +1,82 @@
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
+import {
+  Transfer as TransferEvent,
+  Approval as ApprovalEvent,
+  ApprovalForAll as ApprovalForAllEvent,
+  UriUpdated as UriUpdatedEvent,
+  
+} from "../generated/ERC721/ERC721";
+import { Account, Collection, Token, Transfer, UriUpdate } from "../generated/schema";
+
+function getOrCreateAccount(addr: Bytes): Account {
+  let a = Account.load(addr);
+  if (a == null) { a = new Account(addr); a.balance = BigInt.zero(); }
+  return a as Account;
+}
+
+function getOrCreateCollection(addr: Bytes): Collection {
+  let c = Collection.load(addr);
+  if (c == null) {
+    c = new Collection(addr);
+    c.totalSupply = BigInt.zero();
+  }
+  return c as Collection;
+}
+
+export function handleTransfer(e: TransferEvent): void {
+  const col = getOrCreateCollection(e.address);
+  const tokenId = e.params.tokenId.toString();
+
+  let token = Token.load(tokenId);
+  let from = getOrCreateAccount(e.params.from);
+  let to = getOrCreateAccount(e.params.to);
+
+  if (token == null) {
+    token = new Token(tokenId);
+    token.collection = col.id;
+    token.mintedAt = e.block.timestamp;
+  }
+
+  if (from.id != to.id) {
+    from.balance = from.balance.minus(BigInt.fromI32(1));
+    to.balance = to.balance.plus(BigInt.fromI32(1));
+  }
+
+  token.owner = to.id;
+
+  const id = e.transaction.hash.toHex() + "-" + e.logIndex.toString();
+  const t = new Transfer(id);
+  t.token = token.id;
+  t.from = from.id;
+  t.to = to.id;
+  t.txHash = e.transaction.hash;
+  t.blockNumber = e.block.number;
+  t.timestamp = e.block.timestamp;
+
+  from.save(); to.save(); (token as Token).save(); (col as Collection).save(); t.save();
+}
+
+export function handleApproval(_e: ApprovalEvent): void {}
+
+export function handleApprovalForAll(_e: ApprovalForAllEvent): void {}
+
+export function handleUriUpdated(e: UriUpdatedEvent): void {
+  const tokenId = e.params.agentId.toString();
+  const id = e.transaction.hash.toHex() + "-" + e.logIndex.toString();
+  const u = new UriUpdate(id);
+  u.token = tokenId;
+  u.newUri = e.params.newUri;
+  u.updatedBy = e.params.updatedBy;
+  u.txHash = e.transaction.hash;
+  u.blockNumber = e.block.number;
+  u.timestamp = e.block.timestamp;
+  u.save();
+
+  let token = Token.load(tokenId);
+  if (token != null) {
+    token.uri = e.params.newUri;
+    token.save();
+  }
+}
+
+
---
+++ src/mapping.ts
@@ -0,0 +1,58 @@
+import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
+import { Transfer as TransferEvent, ERC721 } from "../generated/ERC721/ERC721";
+import { Account, Collection, Token, Transfer } from "../generated/schema";
+
+function getOrCreateAccount(addr: Bytes): Account {
+  let a = Account.load(addr);
+  if (a == null) { a = new Account(addr); a.balance = BigInt.zero(); }
+  return a as Account;
+}
+
+function getOrCreateCollection(addr: Bytes): Collection {
+  let c = Collection.load(addr);
+  if (c == null) {
+    c = new Collection(addr);
+    c.totalSupply = BigInt.zero();
+    // Optional: bind contract to read name/symbol once. Avoid repeated eth_calls in handlers.
+    // const contract = ERC721.bind(addr as Address); // use sparingly for performance
+  }
+  return c as Collection;
+}
+
+export function handleTransfer(e: TransferEvent): void {
+  const col = getOrCreateCollection(e.address);
+  const tokenId = e.params.tokenId.toString();
+
+  let token = Token.load(tokenId);
+  let from = getOrCreateAccount(e.params.from);
+  let to = getOrCreateAccount(e.params.to);
+
+  if (token == null) {
+    token = new Token(tokenId);
+    token.collection = col.id;
+    token.mintedAt = e.block.timestamp;
+    // Optional: read tokenURI once; cache it, avoid per-transfer calls
+    // const contract = ERC721.bind(e.address);
+    // let uri = contract.try_tokenURI(e.params.tokenId);
+    // if (!uri.reverted) token.uri = uri.value;
+  }
+
+  // naive balances (fine for most UIs)
+  if (from.id != to.id) {
+    from.balance = from.balance.minus(BigInt.fromI32(1));
+    to.balance = to.balance.plus(BigInt.fromI32(1));
+  }
+
+  token.owner = to.id;
+
+  const id = e.transaction.hash.toHex() + "-" + e.logIndex.toString();
+  const t = new Transfer(id);
+  t.token = token.id;
+  t.from = from.id;
+  t.to = to.id;
+  t.txHash = e.transaction.hash;
+  t.blockNumber = e.block.number;
+  t.timestamp = e.block.timestamp;
+
+  from.save(); to.save(); (token as Token).save(); (col as Collection).save(); t.save();
+}
